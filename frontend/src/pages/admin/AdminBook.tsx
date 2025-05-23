96c75a2ef544194e199392449a0f725b91153cdb
devAmaresh <amprasad.2003@gmail.com>
2025-05-23 14:51:45 +0530

    build fix

--- frontend/src/pages/admin/AdminBook.tsx
+++ frontend/src/pages/admin/AdminBook.tsx
@@ -3,9 +3,11 @@ import { useParams } from "react-router-dom";
 import VehicleForm from "@/components/admin/VehicleForm";
 
 const AdminBook = () => {
+  let id1 = "1";
   const { id } = useParams();
+  id1 = id ? id : id1;
 
-  return <VehicleForm parkingLotId={id} />;
+  return <VehicleForm parkingLotId={id1} />;
 };
 
 export default AdminBook;
